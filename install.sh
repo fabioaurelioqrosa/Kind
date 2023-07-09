b25a88409f5e830830d2ac8c8b29eda31382c529
Fábio Rosa <quintellarosa@gmail.com>
2023-07-09 11:39:12 -0400

    First commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+sudo dnf install -y kubectl
+
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+kind create cluster
+
+kubectl config set-context kind-kind
+kubectl cluster-info
